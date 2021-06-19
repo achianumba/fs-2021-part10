bfe2547ea2737564ac1067a779f5c918d0bdb2a8
achianumba <chianumbaarinze@gmail.com>
2021-06-19 18:54:18 +0100

    10.3

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+import Text from "./Text";
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    // ...
+  },
+  // ...
+});
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+      <Text fontWeight="bold" fontSize="subheading">
+        Repositories
+      </Text>
+    </View>
+  );
+};
+
+export default AppBar;
