9e4eaacf8497fb61a2385faedaf5e20486b416ff
achianumba <chianumbaarinze@gmail.com>
2021-06-19 23:18:56 +0100

    10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 import AppBarTab from "./AppBarTab";
 import theme from "../theme";
@@ -18,10 +18,10 @@ const styles = StyleSheet.create({
 
 const AppBar = () => {
   return (
-    <View style={styles.container}>
+    <ScrollView horizontal style={styles.container}>
       <AppBarTab text="Repositories" to="/" />
       <AppBarTab text="Sign in" to="sign-in" />
-    </View>
+    </ScrollView>
   );
 };
 
